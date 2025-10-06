8e400834f4480911e53058680d449ac178c25352
mcgregr22 <grejoe045@gmail.com>
2025-10-07 01:06:58 +0700

    Simpan perubahan lokal sebelum melakukan pull

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,16 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
     return view('home');
 });
 
-// Route::get('/home', function () {
-//     return view('home');
-// });
-
 Route::get('/register', function () {
     return view('register');
+});
+Route::get('/login', function () {
+    return view('login');
 });
