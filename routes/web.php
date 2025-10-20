d72273bd4f2ac03fa860aef6780bd31758e019ed
Bradleyyx2 <bradleyaditya2705@gmail.com>
2025-10-20 14:46:15 +0700

    Fix konflik Joe, simpan versi saya

--- routes/web.php
+++ routes/web.php
@@ -4,48 +4,46 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrasiController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomepageController;
-use App\Http\Controllers\KeranjangController;
+use App\Http\Controllers\HomePageAdminController;
 
 // ----------------------
-// HALAMAN AWAL (Guest/Public)
+// HALAMAN AWAL
 // ----------------------
 Route::get('/', function () {
-    return view('welcome'); // halaman awal sebelum login
+    return view('welcome');
 });
 
 // ----------------------
 // ROUTE UNTUK TAMU (BELUM LOGIN)
 // ----------------------
 Route::middleware('guest')->group(function () {
-    // REGISTER
     Route::get('/register', [RegistrasiController::class, 'show'])->name('register.show');
     Route::post('/register', [RegistrasiController::class, 'store'])->name('register.store');
 
-    // LOGIN
     Route::get('/login', [LoginController::class, 'show'])->name('login.show');
     Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 });
 
 // ----------------------
-// ROUTE UNTUK USER YANG SUDAH LOGIN (AUTH)
+// ROUTE UNTUK USER LOGIN (AUTH)
 // ----------------------
 Route::middleware('auth')->group(function () {
 
-    // HOMEPAGE DINAMIS
+    // HOMEPAGE USER
     Route::get('/homepage', [HomepageController::class, 'index'])->name('homepage');
 
-    // =========================
-    // KERANJANG 
-    // =========================
-    Route::middleware('auth')->group(function () {
-
-    Route::get('/keranjang', [KeranjangController::class, 'index'])->name('cart.index');
-    Route::post('/keranjang/tambah', [KeranjangController::class, 'add'])->name('cart.add');
-    Route::delete('/keranjang/hapus/{id}', [KeranjangController::class, 'remove'])->name('cart.remove');
+    // HOMEPAGE ADMIN
+    // ROUTE UNTUK ADMIN
+    Route::get('/homepage_admin', [HomePageAdminController::class, 'index'])->name('homepage_admin');
+    Route::post('/homepage_admin/tambah', [HomePageAdminController::class, 'store'])->name('homepage_admin.store');
+    Route::delete('/homepage_admin/hapus/{id}', [HomePageAdminController::class, 'destroy'])->name('homepage_admin.destroy');
 
 
-});
-    // DETAIL BUKU
+    // HALAMAN LAIN
+    Route::get('/swapbook', fn() => view('swapbook'));
+    Route::get('/keranjang', fn() => view('keranjang'));
+    Route::get('/mycollection', fn() => view('mycollection'));
+    Route::get('/forumdiscuss', fn() => view('forumdiscuss'));
     Route::get('/buku/{id}', [HomepageController::class, 'show'])->name('buku.show');
 
     // LOGOUT
