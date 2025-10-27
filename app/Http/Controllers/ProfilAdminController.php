50cb22ed0ce8d30be1974ec7aa8867a37e645c50
Darubram <bdarubramantya@gmail.com>
2025-10-28 00:29:51 +0700

    update

--- app/Http/Controllers/ProfilAdminController.php
+++ app/Http/Controllers/ProfilAdminController.php
@@ -9,7 +9,6 @@ class ProfilAdminController extends Controller
     public function index()
     {
         $admin = auth()->user(); // Ambil user yang sedang login
-
         return view('profil_admin', compact('admin'));
     }
 }
